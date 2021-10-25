5c2d86d3e89f60779ec0a33055e42d396e2278a0
Ole Schönburg <ole.schoenburg@gmail.com>
2021-10-25 15:46:21 +0200

    Install and execute zbctl

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-import { getInput, setOutput, setFailed } from '@actions/core';
-import { context } from '@actions/github';
+import * as core from '@actions/core';
+import * as tc from '@actions/tool-cache';
+import * as gh from '@actions/github';
+import * as exec from '@actions/exec';
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const version = getInput('version');
-  console.log(`Retrieving zbctl ${version}!`);
-  const time = (new Date()).toTimeString();
-  setOutput("result", "{}");
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    const version = core.getInput('version');
+    const command = core.getInput('command');
+
+    const token = process.env['GITHUB_TOKEN']
+    const octo = gh.getOctokit(token);
+    
+    const release = await octo.rest.repos.getReleaseByTag({
+        owner: 'camunda-cloud',
+        repo: 'zeebe',
+        tag: version,
+    });
+    const asset = release.data.assets.find(asset => asset.name == "zbctl");
+    const binPath = await tc.downloadTool(asset.browser_download_url);
+    core.addPath(binPath);
+    const output = await exec.getExecOutput(`zbctl ${command}`);
+    if (output.exitCode != 0) {
+        core.setFailed(`zbctl failed with exit code ${output.exitCode}: ${output.stderr}`)
+    }
+    core.setOutput("result", output.stdout);
 } catch (error) {
-  setFailed(error.message);
+    core.setFailed(error.message);
 }
